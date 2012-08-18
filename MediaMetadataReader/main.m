ea333977186999a42443e180920ae37354152dcd
Douglas Hill <douglas@douglashill.co>
2012-08-18 11:13:46 +0100

    Initial Commit

---
+++ MediaMetadataReader/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MediaMetadataReader
+//
+//  Created by Douglas Hill on 18/08/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
